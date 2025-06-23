a6993f27fa75979a29450efa1ef8377f992fadac
daniel99k <dk99@divcodes.com>
2025-06-23 15:12:01 +0300

    .

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,7 +5,7 @@ import Home from "./pages/home.tsx";
 
 const routers = createBrowserRouter([
     {
-        path: "/portfolio",
+        path: "/",
         element: <MainLayout/>,
         children: [
             {
